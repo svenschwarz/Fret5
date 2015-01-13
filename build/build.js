0ec116f3b48cccbe04f6e62c9ebad5953f2757c3
Sven Schwarz <schwarz@dfki.uni-kl.de>
2015-01-13 09:57:10 +0100

    added build directory for and with optimized js

---
+++ build/build.js
@@ -0,0 +1,16 @@
+({
+    baseUrl: '../js',
+
+    paths: {
+        '3rdparty': 'js/3rdparty',
+        'view': '../view',
+        'app': '../app',
+
+        text: '3rdparty/requirejs/text',
+        jquery: '3rdparty/jquery/jquery-2.1.0',
+        knockout: '3rdparty/knockout/knockout-2.3.0'
+    },
+
+    name: "../main",
+    out: "Fret5-opt.js"
+})
